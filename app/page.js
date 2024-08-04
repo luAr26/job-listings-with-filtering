eab38652ff443d973e9ece98c41ea82a5caa18c0
Raul <raul_cosmin_26@yahoo.com>
2024-08-04 13:53:45 +0300

    Add @svgr/webpack.

--- app/page.js
+++ app/page.js
@@ -1,12 +1,16 @@
 /** @format */
 
 import Attribution from "@/app/_components/Attribution";
+import AccountLogo from "@/public/images/account.svg";
+import TwitchIcon from "@/public/images/twitch.svg";
 
 export default function Home() {
   return (
     <>
       <h1 className="ml-[20rem] text-3xl">Hello, Next.js</h1>
       <Attribution />
+      <AccountLogo />
+      <TwitchIcon />
     </>
   );
 }
