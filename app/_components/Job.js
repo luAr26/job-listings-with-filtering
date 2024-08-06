7fe32cee19ab519fcfb85258a8aea9c25718bb8d
Raul <raul_cosmin_26@yahoo.com>
2024-08-06 21:35:24 +0300

    Make some small adjustments.

--- app/_components/Job.js
+++ app/_components/Job.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { twMerge } from "tailwind-merge";
 
 function Job({ job }) {
   const {
@@ -18,13 +19,20 @@ function Job({ job }) {
   } = job;
 
   return (
-    <div className="desktop:flex desktop:items-center desktop:gap-[1.5rem] shadow-main relative flex-row rounded-[5px] bg-white p-5">
-      <div className="desktop:relative desktop:top-0 desktop:left-0 desktop:w-[88px] desktop:h-[88px] absolute -top-[24px] h-[48px] w-[48px]">
+    <div
+      className={twMerge(
+        "relative flex-row rounded-[5px] bg-white p-5 shadow-main desktop:flex desktop:items-center desktop:gap-[1.5rem]",
+        featured && "border-l-[5px] border-primary",
+      )}
+    >
+      <div className="absolute -top-[24px] h-[48px] w-[48px] desktop:relative desktop:left-0 desktop:top-0 desktop:h-[88px] desktop:w-[88px]">
         <Image src={logo} fill alt={`${company} logo`} priority />
       </div>
       <div className="desktop:mr-auto">
         <div className="mt-4 flex flex-row items-center gap-8">
-          <h2 className="text-[0.8125rem] text-primary">{company}</h2>
+          <h2 className="text-[0.8125rem] text-primary desktop:text-[1.125rem]">
+            {company}
+          </h2>
           <div className="flex flex-row gap-4">
             {firstTime && (
               <span className="rounded-xl bg-primary px-2 pt-1 text-[0.875rem] uppercase leading-[1.4] text-white">
@@ -39,19 +47,19 @@ function Job({ job }) {
           </div>
         </div>
         <div className="mt-2">
-          <h3 className="text-[0.9375rem] leading-[1.6]">
+          <h3 className="text-[0.9375rem] leading-[1.6] desktop:text-[1.375rem] desktop:leading-[1.09090]">
             <Link href="#" className="hover:text-primary">
               {position}
             </Link>
           </h3>
         </div>
-        <div className="mt-2 flex flex-row items-center gap-2 pb-4 font-medium text-secondary">
+        <div className="[flex-row mt-2 flex items-center gap-2 pb-4 font-medium -tracking-[0.12px] text-secondary desktop:text-[1.125rem] desktop:-tracking-[0.14px]">
           <span className="job-metadata">{postedAt}</span>
           <span className="job-metadata">{contract}</span>
           <span>{location}</span>
         </div>
       </div>
-      <ul className="desktop:border-t-0 flex flex-row flex-wrap items-center gap-4 border-t-[1px] border-light pt-4">
+      <ul className="flex flex-row flex-wrap items-center gap-4 border-t-[1px] border-light pt-4 desktop:border-t-0">
         <li className="inline-flex cursor-pointer rounded-[4px] bg-lighter px-2 pb-[0.1875em] pt-[0.3125em] text-[1rem] leading-[1.5] text-primary hover:bg-primary hover:text-white">
           {role}
         </li>
